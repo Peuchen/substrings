6a5e2459b7549911d874e4833705428c8ff3d346
peuchen <npeuchen@gmail.com>
2020-09-27 13:27:11 +0200

    Evaluate each word against the dictionary's substrings, one for one

--- substrings.rb
+++ substrings.rb
@@ -1,4 +1,4 @@
-require 'pry'; binding.pry
+
 
 #Create a method that aks for one or more words as the first argument and an array of substrings (the dictionary) as the second argument
 def substrings(words, dictionary)
@@ -8,14 +8,24 @@ def substrings(words, dictionary)
 
 #Split the first argument
   words.split(' ').each do |word|
-  end
 
-end
 #Check the first argument's words on the presence of the substrings in the second argument
+    dictionary.each do |substring|
+      if word.include?(substring)
+        if results.key?(substring)
+          results[substring] += 1
+        else
+          results[substring] = 1
+        end
+      end
+    end
+  end
+  results
+end
 #If the substring is not present in the hash, add it and assign '1' to it
 #If the substring is already present, add 1 to its assigned number in the hash
 #When all of the words are evaluated, return the hash
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
-substrings("below", dictionary)
+p substrings("Howdy partner, sit down! How's it going?", dictionary)
