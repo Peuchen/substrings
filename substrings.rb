78059abc35d36da785e6b08251de28806860ce2c
peuchen <npeuchen@gmail.com>
2020-09-27 13:07:44 +0200

    Write out the pseudocode

---
+++ substrings.rb
@@ -0,0 +1,9 @@
+require 'pry'
+
+#Create a method that aks for one or more words as the first argument and an array of substrings (the dictionary) as the second argument
+#Create a hash
+#Split the first argument
+#Check the first argument's words on the presence of the substrings in the second argument
+#If the substring is not present in the hash, add it and assign '1' to it
+#If the substring is already present, add 1 to its assigned number in the hash
+#When all of the words are evaluated, return the hash
